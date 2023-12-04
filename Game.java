01619f690b5421017e666e200bd46f746ed7fa8e
cbacary <cbacary4@gmail.com>
2023-12-03 19:08:23 -0500

    Added reraising checks

--- Game.java
+++ Game.java
@@ -1,5 +1,7 @@
 public class Game {
     public static void main(String[] args) {
-        
+        GameMaster gm = new GameMaster(6, 1000);
+
+        gm.playNextPhase();
     }
 }
--- GameMaster.java
+++ GameMaster.java
@@ -14,6 +14,9 @@ public class GameMaster {
     private ArrayList<Card> boardCards;
     private Deck deck;
 
+    // Stores the index of which player is the final player to place a bet
+    private int endPlayer;
+
     private int pot;
     private int currentRaise;
     private int cardsInPlay;
@@ -49,6 +52,8 @@ public class GameMaster {
             dealCards();
             getBets();
         }
+
+        currentPhase += 1;
     }
 
     private void postBlinds() {
@@ -71,55 +76,106 @@ public class GameMaster {
         }
     }
 
+    /** 
+     * Iterates through players in correct order getting each of their bets / 
+     * decissions
+     * */
     private void getBets() {
-
         int playerIndex = (dealer + 3) % players.size();
+        // endPlayer is how we control where we stop asking for bets, it will 
+        // be changed by playerRaise if that player is performing a re-raise
+        endPlayer = (dealer + 3) % players.size();
         do {
-            if (playersInGame.get(playerIndex) == false) {
+
+            if (!playersInGame.get(playerIndex)) {
+                playerIndex += 1;
                 continue;
             }
+
             Player player = players.get(playerIndex);
-            System.out.printf("board: %s\tpot: %d\traise: %d", boardToString(),
-                              pot, currentRaise);
-            System.out.printf("Player %d cards: %s", playerIndex,
-                              player.toString());
-            System.out.printf("Player %d ('r'aise, 'c'all, 'f'old): ",
-                              playerIndex);
-            
-        } while (playerIndex != (dealer + 3) % players.size());
-    }
+            printMenu(player, playerIndex);
 
-    private void userAction(Player player, int playerIndex) {
-        String choice = scanner.nextLine();
-            switch (choice) {
-            case "r":
-                if (currentRaise - player.getMoneyEntered() >
-                    player.getMoney()) {
-                    System.out.println(
-                        "You do not have enough money to raise. You can either call or fold.");
-                    continue;
-                }
-                playerRaise(player);
-                break;
-            case "c":
-                playerCall(player);
-                break;
-            case "f":
-                playerFold(playerIndex);
-                break;
-            default:
-                System.out.println("Invalid choice. Try again.");
-                continue;
+            // if the user made an acceptable decision, increment playerIndex
+            if (userAction(player, playerIndex)) {
+                playerIndex += 1;
             }
+            // Otherwise don't increment and just rerun loop
+        } while (playerIndex != endPlayer);
     }
 
-    private void playerRaise(Player player) {
+    /**
+     * Checks if a player raised off a raise. Important because then we have to
+     * reask what previous players want to do. Should only be called after all
+     * players have made their move
+     * @return returns true if a player raised off a raise
+     * */
+    private boolean playerReRaised() {
+        for (int i = 0; i < players.size(); ++i) {
+            if (!playersInGame.get(i))
+                continue;
+            Player player = players.get(i);
+            if (player.getMoneyEntered() != currentRaise &&
+                player.getMoney() != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void printMenu(Player player, int playerIndex) {
+        System.out.printf("board: %s\tpot: %d\traise: %d", boardToString(), pot,
+                          currentRaise);
+        System.out.printf("Player %d cards: %s", playerIndex,
+                          player.toString());
+        System.out.printf("Player %d ('r'aise, 'c'all, 'f'old): ", playerIndex);
+    }
+
+    private boolean userAction(Player player, int playerIndex) {
+        String choice = scanner.nextLine();
+        switch (choice) {
+        case "r":
+            if (currentRaise - player.getMoneyEntered() > player.getMoney()) {
+                System.out.println(
+                    "You do not have enough money to raise. You can either call or fold.");
+                return false;
+            }
+            if (!playerRaise(player, playerIndex)) {
+                System.out.println("You cannot raise by this amount.");
+                return false;
+            }
+            break;
+        case "c":
+            playerCall(player);
+            break;
+        case "f":
+            playerFold(playerIndex);
+            break;
+        default:
+            System.out.println("Invalid choice. Try again.");
+            return false;
+        }
+        return true;
+    }
+
+    // WARNING: I think there are some bugs in this, some infinite loops that 
+    // could happen without prior checks so careful when re-writing for GUI. 
+    // Specifically, regarding the reRaisedIndex
+    private boolean playerRaise(Player player, int playerIndex) {
         System.out.printf("raise by : %d + ", currentRaise);
         int amount = scanner.nextInt();
-
         amount = player.raise(amount);
-        currentRaise += amount;
-        pot += amount;
+
+        if (amount > 0) {
+            // If this is a re-raise, then the round should end only once this
+            // player is reached again, providing other players with their 
+            // opportunity to respond.
+            endPlayer = (currentRaise > 0 ? playerIndex : endPlayer);
+            currentRaise += amount;
+            pot += amount;
+            return true;
+        }
+        System.out.println("Invalid raise amount. Please try again");
+        return false;
     }
 
     private void playerCall(Player player) {
