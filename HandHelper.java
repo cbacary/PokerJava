57eb97714453a3099b3ffee4925b35c30bdc7359
cbacary <cbacary4@gmail.com>
2023-12-04 17:54:10 -0500

    Almost there

--- HandHelper.java
+++ HandHelper.java
@@ -1,126 +1,64 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class HandHelper {
 
     private static final int ROYAL_RANKS =
-        Rank.TEN.getValue() + Rank.JACK.getValue() + Rank.QUEEN.getValue() +
-        Rank.KING.getValue() + Rank.ACE.getValue();
+        Rank.TEN.getValue() | Rank.JACK.getValue() | Rank.QUEEN.getValue() |
+        Rank.KING.getValue() | Rank.ACE.getValue();
 
     public static Hand getHand(ArrayList<Card> board, ArrayList<Card> player) {
         ArrayList<Card> cards = new ArrayList<Card>();
         cards.addAll(board);
         cards.addAll(player);
 
-        ArrayList<Card> sortedByRank = new ArrayList<>(cards);
-        ArrayList<Card> sortedBySuit = new ArrayList<>(cards);
 
-        Collections.sort(sortedByRank, Card.compareByRank);
-        Collections.sort(sortedBySuit, Card.compareBySuit);
+        ArrayList<ArrayList<Card>> cardsByRank = cardsByRank(cards);
+        ArrayList<ArrayList<Card>> cardsBySuit = cardsBySuit(cards);
 
-        if (isRoyal(sortedByRank)) {
-            return Hand.ROYAL_FLUSH;
-        }
+
+        Hand bestHand = Hand.HIGH_CARD;
+
+        Hand h = getStraights(cardsByRank);
+        bestHand = (bestHand.getValue() < h.getValue() ? h : bestHand);
+
+        h = getPairs(cardsByRank);
+        bestHand = (bestHand.getValue() < h.getValue() ? h : bestHand);
+
+        h = getFlush(cardsBySuit);
+        bestHand = (bestHand.getValue() < h.getValue() ? h : bestHand);
+
+        return bestHand;
+
     }
 
     public static Hand getPairs(ArrayList<ArrayList<Card>> cardsByRank) {
+
+        // Loop through cards sorted by rank and get the highest pair count and 
+        // the next highest pair count.
+        int highestPairCount = 0;
+        int nextPairCount = 0;
+
         // We start at index 1 because the first and last index are both ace
-        boolean threeKind = false;
-        boolean pair = false;
-
         for (int rank = 1; rank < cardsByRank.size(); ++rank) {
-            ArrayList<Card> cards = cardsByRank.get(rank);
-            if (cards.size() == 4) {
-                return Hand.FOUR_KIND;
-            } else if ()
-        }
-    }
-
-    public static boolean isFullHouse(ArrayList<Card> cards) {
-
-        // Sort the cards by rank so cards of same rank are organized
-        Collections.sort(cards, Card.compareByRank);
-
-        // The ranks of the cards that make up the three of a kind and pair
-        Rank threeRank = null;
-        Rank twoRank = null;
-
-        int i = 0;
-        while (i < cards.size()) {
-            int j = i + 1;
-            Rank rankToCheck = cards.get(i).getRank();
-            while (j < cards.size()) {
-                if (cards.get(j).getRank() != rankToCheck) {
-                    break;
-                }
-                j += 1;
-            }
-
-            int numOfRank = j - i;
-            if (numOfRank >= 3) {
-                if (threeRank != null) {
-                    twoRank = rankToCheck;
-                    return true;
-                }
-                threeRank = rankToCheck;
-            } else if (numOfRank >= 2) {
-                twoRank = rankToCheck;
-            }
-
-            i = j;
-        }
-
-        return (threeRank != null && twoRank != null && twoRank != threeRank);
-    }
-
-    public static boolean isPair(ArrayList<Card> cards, int pairCount) {
-        Collections.sort(cards, Card.compareByRank);
-
-        // Now iterate through the sorted cards and count many ranks in a row
-        int sameRanks = 1;
-        Rank lastRank = cards.get(0).getRank();
-        for (int i = 1; i < cards.size(); ++i) {
-            if (cards.get(i).getRank() == lastRank) {
-                sameRanks += 1;
-                if (sameRanks == pairCount)
-                    return true;
-            } else {
-                lastRank = cards.get(i).getRank();
-                sameRanks = 1;
+            if (cardsByRank.get(rank).size() >= highestPairCount) {
+                nextPairCount = highestPairCount;
+                highestPairCount = cardsByRank.get(rank).size();
             }
         }
 
-        return sameRanks == pairCount;
-    }
-
-    public static boolean isRoyal(ArrayList<Card> cards) {
-        // We sort by the suit
-        Collections.sort(cards, Card.compareBySuit);
-
-        // Iterate through the sorted cards
-        int cardsSum = 0;
-        Suit lastSuit = cards.get(0).getSuit();
-        for (int i = 1; i < cards.size(); ++i) {
-
-            Suit suit = cards.get(i).getSuit();
-
-            // If the suit is the same add it to the sum
-            if (suit == lastSuit) {
-                cardsSum = cardsSum | cards.get(i).getRank().getValue();
-                if ((cardsSum & ROYAL_RANKS) == ROYAL_RANKS) {
-                    // If the suit is not the same check if the last suit
-                    // cardSum equals ROYAL_RANKS. if it did then we return true
-                    return true;
-                }
-            } else {
-                cardsSum = 0;
-            }
-
-            lastSuit = suit;
+        if (highestPairCount == 4) {
+            return Hand.FOUR_KIND;
+        } else if (highestPairCount >= 3 && nextPairCount >= 3) {
+            return Hand.FULL_HOUSE;
+        } else if (highestPairCount == 3) {
+            return Hand.THREE_KIND;
+        } else if (highestPairCount == 2 && nextPairCount == 2) {
+            return Hand.TWO_PAIR;
+        } else if (highestPairCount == 2) {
+            return Hand.PAIR;
         }
 
-        return (cardsSum & ROYAL_RANKS) == ROYAL_RANKS;
+        return Hand.HIGH_CARD;
     }
 
     /**
@@ -207,17 +145,15 @@ public class HandHelper {
                     : bestHand);
     }
 
-    public static boolean isFlush(ArrayList<Card> cards) {
+    public static Hand getFlush(ArrayList<ArrayList<Card>> cardsBySuit) {
 
-        ArrayList<ArrayList<Card>> cardsBySuit = cardsBySuit(cards);
-
-        for (ArrayList<Card> cardsForSuit: cardsBySuit) {
+        for (ArrayList<Card> cardsForSuit : cardsBySuit) {
             if (cardsForSuit.size() >= 5) {
-                return true;
+                return Hand.FLUSH;
             }
         }
 
-        return false;
+        return Hand.HIGH_CARD;
     }
 
     public static ArrayList<ArrayList<Card>>
@@ -226,7 +162,7 @@ public class HandHelper {
         ArrayList<ArrayList<Card>> cardsBySuit =
             new ArrayList<ArrayList<Card>>(Suit.values().length);
 
-        for (Card card: cards) {
+        for (Card card : cards) {
             Suit s = card.getSuit();
             cardsBySuit.get(s.getValue()).add(card);
         }
